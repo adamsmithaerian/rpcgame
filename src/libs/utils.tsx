bafddace22fbe334c4f29c8e449b9842cfd17037
Adam Smith <adam.smith@aerian.com>
2018-03-29 11:55:51 +0100

    optomized random gen

--- src/libs/utils.tsx
+++ src/libs/utils.tsx
@@ -1,7 +1,7 @@
 import { Shape, Result } from '../consts/types';
 
 export const getRandomShape = (): Shape =>
-  Math.floor(Math.random() * Math.floor(3));
+  Math.round(Math.random() * 2);
 
 export const calculateRPSResult = (user: Shape, computer: Shape): Result => {
   const resultGrid: Result[] = [
